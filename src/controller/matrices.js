08f13b62060f2c4b73d8610a5bea1c56154458ac
jimalaros <jimalaros25@gmail.com>
2022-11-23 13:12:39 -0500

    first commit

--- src/controller/matrices.js
+++ src/controller/matrices.js
@@ -3,10 +3,8 @@ const { generarTranspuesta } = require("../utils/operaciones");
 exports.matrices = (req, res) => {
     let { url } = req;
 
-    console.log(url);
     let query = url.split("?")[1];
     let n = query.split("=")[1];
-    console.log(n)
 
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.write(generarTranspuesta(n));
