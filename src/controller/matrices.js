3f39d4f205c870087a09bf6a02e60e1287b07e8d
jimalaros <jimalaros25@gmail.com>
2022-11-23 16:55:33 -0500

    first commit

--- src/controller/matrices.js
+++ src/controller/matrices.js
@@ -1,12 +1,16 @@
+const { typeOf } = require("mathjs");
 const { generarTranspuesta } = require("../utils/operaciones");
 
 exports.matrices = (req, res) => {
     let { url } = req;
 
-    let query = url.split("?")[1];
-    let n = query.split("=")[1];
+    let n = url.split("/")[1];
 
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write(generarTranspuesta(n));
-    res.end();
+    if(n!='favicon.ico') {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.write(generarTranspuesta(n));
+        res.end();
+    }
+    
+    
 };
--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const { server } = require("./server");
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+    console.log(`App running on port ${ PORT }`);
 });
