3c5b4131570b932d4ee1a3b27af1cb9862ed40e1
jimalaros <jimalaros25@gmail.com>
2022-11-24 09:30:32 -0500

    first commit

--- src/controller/matrices.js
+++ src/controller/matrices.js
@@ -7,10 +7,10 @@ exports.matrices = (req, res) => {
     let n = url.split("/")[1];
 
     if(n!='favicon.ico') {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-        res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-setHeaders', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+        res.setHeader('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
         res.writeHead(200, { "Content-Type": "application/json" });
         res.write(generarTranspuesta(n));
         res.end();
