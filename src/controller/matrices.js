b10fe7d42c98d22aee142d34411caf2f1820a3c3
jimalaros <jimalaros25@gmail.com>
2022-11-24 09:20:44 -0500

    first commit

--- src/controller/matrices.js
+++ src/controller/matrices.js
@@ -7,6 +7,10 @@ exports.matrices = (req, res) => {
     let n = url.split("/")[1];
 
     if(n!='favicon.ico') {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+        res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
         res.writeHead(200, { "Content-Type": "application/json" });
         res.write(generarTranspuesta(n));
         res.end();
--- src/server.js
+++ src/server.js
@@ -9,6 +9,10 @@ exports.server = http.createServer((req, res) => {
     switch (method) {
         case "GET":
             if (url === "/") {
+                res.header('Access-Control-Allow-Origin', '*');
+                res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+                res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+                res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
                 res.writeHead(200, { "Content-Type": "text/plain" });
                 res.write("Bienvenido");
                 res.end();
